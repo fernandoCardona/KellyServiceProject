234eb4d8648c9add6f2608aacba6b1ded17eef0d
Fernando Cardona <fernandocardona@MacBook-Pro-de-Fernando.local>
2021-11-24 16:35:31 +0100

    before pull

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -27,22 +27,23 @@ function startMap() {
 
 function placeMarkers(map) {
 
-  const myMarker = new google.maps.Marker({
-    position: {
-      lat: 41.3977381,
-      lng: 2.190471916
-    },
-    map: map,
-    title: "Barcelona Campus"
-  });
-
-  new google.maps.Marker({
-    position: {
-      lat: 41.4277381,
-      lng: 2.190471916
-    },
-    map: map,
-    title: "Barcelona Campus"
+  axios.get("/services/api")
+  .then((res) => {
+    console.log(res)  
+    
+  })
+  .catch(error => {
+    console.log(error)
+    res.render('message', { errorMessage: "Marker no ha podido ser enviado" })
+  })
+  
+  map.addMarker({
+    lat: -12.043333,
+    lng: -77.028333,
+    title: 'Lima',
+    click: function(e){
+      alert('You clicked in this marker');
+    }
   });
 
 
@@ -80,63 +81,3 @@ function setUserCenter(map) {
 
 
 
-// ///////Geting current location for devices //////
-// // Note: This example requires that you consent to location sharing when
-// // prompted by your browser. If you see the error "The Geolocation service
-// // failed.", it means you probably did not give permission for the browser to
-// // locate you.
-// let map: google.maps.Map, infoWindow: google.maps.InfoWindow;
-
-// function initMap(): void {
-//   map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-//     center: { lat: -34.397, lng: 150.644 },
-//     zoom: 6,
-//   });
-//   infoWindow = new google.maps.InfoWindow();
-
-//   const locationButton = document.createElement("button");
-
-//   locationButton.textContent = "Pan to Current Location";
-//   locationButton.classList.add("custom-map-control-button");
-
-//   map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-
-//   locationButton.addEventListener("click", () => {
-//     // Try HTML5 geolocation.
-//     if (navigator.geolocation) {
-//       navigator.geolocation.getCurrentPosition(
-//         (position: GeolocationPosition) => {
-//           const pos = {
-//             lat: position.coords.latitude,
-//             lng: position.coords.longitude,
-//           };
-
-//           infoWindow.setPosition(pos);
-//           infoWindow.setContent("Location found.");
-//           infoWindow.open(map);
-//           map.setCenter(pos);
-//         },
-//         () => {
-//           handleLocationError(true, infoWindow, map.getCenter()!);
-//         }
-//       );
-//     } else {
-//       // Browser doesn't support Geolocation
-//       handleLocationError(false, infoWindow, map.getCenter()!);
-//     }
-//   });
-// }
-
-// function handleLocationError(
-//   browserHasGeolocation: boolean,
-//   infoWindow: google.maps.InfoWindow,
-//   pos: google.maps.LatLng
-// ) {
-//   infoWindow.setPosition(pos);
-//   infoWindow.setContent(
-//     browserHasGeolocation
-//       ? "Error: The Geolocation service failed."
-//       : "Error: Your browser doesn't support geolocation."
-//   );
-//   infoWindow.open(map);
-// }
